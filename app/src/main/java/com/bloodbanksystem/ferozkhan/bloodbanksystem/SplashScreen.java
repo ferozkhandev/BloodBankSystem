8dfe1add6276c8131fa975279503143bd347b459
Feroz Khan <khan.feroz8899@gmail.com>
2018-04-06 19:44:14 +0500

    Addition of Splash Screen.

--- app/src/main/java/com/bloodbanksystem/ferozkhan/bloodbanksystem/SplashScreen.java
+++ app/src/main/java/com/bloodbanksystem/ferozkhan/bloodbanksystem/SplashScreen.java
@@ -1,9 +1,21 @@
 package com.bloodbanksystem.ferozkhan.bloodbanksystem;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
+import com.shashank.sony.fancydialoglib.FancyAlertDialogListener;
+import com.shashank.sony.fancydialoglib.Icon;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,14 +23,96 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable(){
-            @Override
+        splash();
+        if(isWorkingInternetPersent())
+        {
+            splash();
+        }
+        else
+        {
+            showAlertDialog(SplashScreen.this, "Internet Connection",
+                    "You don't have internet connection", false);
+        }
+    }
+
+    public void splash() {
+        Thread timerTread = new Thread() {
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
-                SplashScreen.this.startActivity(mainIntent);
-                SplashScreen.this.finish();
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    SplashScreen.this.startActivity(mainIntent);
+                    SplashScreen.this.finish();
+                }
             }
-        }, 4000);
+        };
+        timerTread.start();
+    }
+
+    public boolean isWorkingInternetPersent() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getBaseContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+
+    public void showAlertDialog(Context context, String title, String message, Boolean status) {
+//        final AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+//
+//        // Setting Dialog Title
+//        alertDialog.setTitle(title);
+//
+//        // Setting Dialog Message
+//        alertDialog.setMessage(message);
+//
+//        // Setting alert dialog icon
+//        // alertDialog.setIcon((status) ? R.mipmap.ic_launcher : R.mipmap.ic_launcher);
+//
+//        // Setting OK Button
+//        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int which) {
+//
+//                alertDialog.dismiss();
+//                //System.exit(0);
+//            }
+//        });
+//
+//        // Showing Alert Message
+//        alertDialog.show();
+        new FancyAlertDialog.Builder(this)
+                .setTitle(title)
+                .setBackgroundColor(Color.parseColor("#D22E2E"))  //Don't pass R.color.colorvalue
+                .setMessage(message)
+                .setNegativeBtnText("Cancel")
+                .setPositiveBtnBackground(Color.parseColor("#FF4081"))  //Don't pass R.color.colorvalue
+                .setPositiveBtnText("OK")
+                .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))  //Don't pass R.color.colorvalue
+                .setAnimation(Animation.SIDE)
+                .isCancellable(true)
+                //.setIcon(R.drawable.ic_star_border_black_24dp, Icon.Visible)
+                .OnPositiveClicked(new FancyAlertDialogListener() {
+                    @Override
+                    public void OnClick() {
+                        Toast.makeText(getApplicationContext(),"Please check your internet connection", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .OnNegativeClicked(new FancyAlertDialogListener() {
+                    @Override
+                    public void OnClick() {
+                        //Toast.makeText(getApplicationContext(),"Cancel", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .build();
     }
 }

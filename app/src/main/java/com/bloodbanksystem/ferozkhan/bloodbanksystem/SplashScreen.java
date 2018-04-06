c89db2f4ec4d355413efe93718181cb38fe0757d
Feroz Khan <khan.feroz8899@gmail.com>
2018-04-06 22:50:00 +0500

    Addition of Splash Screen.

--- app/src/main/java/com/bloodbanksystem/ferozkhan/bloodbanksystem/SplashScreen.java
+++ app/src/main/java/com/bloodbanksystem/ferozkhan/bloodbanksystem/SplashScreen.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -23,7 +24,6 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        splash();
         if(isWorkingInternetPersent())
         {
             splash();
@@ -94,25 +94,44 @@ public class SplashScreen extends AppCompatActivity {
                 .setTitle(title)
                 .setBackgroundColor(Color.parseColor("#D22E2E"))  //Don't pass R.color.colorvalue
                 .setMessage(message)
-                .setNegativeBtnText("Cancel")
+                .setNegativeBtnText("Settings")
                 .setPositiveBtnBackground(Color.parseColor("#FF4081"))  //Don't pass R.color.colorvalue
-                .setPositiveBtnText("OK")
+                .setPositiveBtnText("Exit")
                 .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))  //Don't pass R.color.colorvalue
-                .setAnimation(Animation.SIDE)
+                .setAnimation(Animation.POP)
                 .isCancellable(true)
                 //.setIcon(R.drawable.ic_star_border_black_24dp, Icon.Visible)
                 .OnPositiveClicked(new FancyAlertDialogListener() {
                     @Override
                     public void OnClick() {
-                        Toast.makeText(getApplicationContext(),"Please check your internet connection", Toast.LENGTH_SHORT).show();
+                        System.exit(0);
+                        //Toast.makeText(getApplicationContext(),"Please check your internet connection", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .OnNegativeClicked(new FancyAlertDialogListener() {
                     @Override
                     public void OnClick() {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                        try {
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        finally {
+                            if(isWorkingInternetPersent())
+                            {
+                                splash();
+                            }
+                            else
+                            {
+                                showAlertDialog(SplashScreen.this, "Internet Connection",
+                                        "You don't have internet connection", false);
+                            }
+                        }
                         //Toast.makeText(getApplicationContext(),"Cancel", Toast.LENGTH_SHORT).show();
                     }
                 })
+                
                 .build();
     }
 }

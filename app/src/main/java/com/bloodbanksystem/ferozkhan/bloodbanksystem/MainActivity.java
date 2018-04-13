1cf7b662a904ce717bb06006db985d8f2fa628f4
Feroz Khan <khan.feroz8899@gmail.com>
2018-04-13 18:03:34 +0500

    Need Blood Icon

--- app/src/main/java/com/bloodbanksystem/ferozkhan/bloodbanksystem/MainActivity.java
+++ app/src/main/java/com/bloodbanksystem/ferozkhan/bloodbanksystem/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
